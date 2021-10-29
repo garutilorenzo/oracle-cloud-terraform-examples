824fc0800d360c1a6665336b953e8ed7b288896d
Lorenzo Garuti <garuti.lorenzo@gmail.com>
2021-10-29 17:14:22 +0200

    Fix output

--- instance-pool/output.tf
+++ instance-pool/output.tf
@@ -2,7 +2,7 @@ output "instances_ips" {
   depends_on = [
     data.oci_core_instance_pool_instances.ubuntu_instance_pool_instances,
   ]
-  value = data.oci_core_instance.ubuntu_instance_pool_instances_ips.*.private_ip
+  value = data.oci_core_instance.ubuntu_instance_pool_instances_ips.*.public_ip
 }
 
 output "lb_ip" {
